aeab1f3d17c9c48ebb191a14e6a02b076308bcf0
Arthur Asanaliev <arthis1707@gmail.com>
2024-07-03 17:13:20 +0600

    added GetMove method

--- game/player.go
+++ game/player.go
@@ -1,5 +1,7 @@
 package game
 
+import "fmt"
+
 type Player struct {
 	name  string
 	color string
@@ -10,4 +12,13 @@ func NewPlayer(name, color string) *Player {
 		name:  name,
 		color: color,
 	}
+}
+
+func (p *Player) GetMove() (int, int, int, int) {
+	var xFrom, yFrom, xTo, yTo int
+	fmt.Printf("%s, choose a piece you want to move: ")
+	fmt.Scanf("%d %d", &xFrom, &yFrom)
+	fmt.Print("Now choose a cell you want to move to: ")
+	fmt.Scanf("%d %d", &xTo, &yTo)
+	return xFrom, yFrom, xTo, yTo
 }
